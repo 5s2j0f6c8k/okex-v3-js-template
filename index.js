9945f9f227fe44888ac9488bdf191b481b7b9dcb
qugang <qgass@163.com>
2018-10-27 03:07:17 +0800

    添加其他模板

--- index.js
+++ index.js
@@ -10,12 +10,43 @@ log4js.configure({
     categories: { default: { appenders: ['console'], level: 'all' } }
 })
 let loggger = log4js.getLogger()
-loggger.debug("hello world")
 
 
 let restWalletApi = new okex.RestWalletApi("https://www.okex.com", "", "", "")
 
 restWalletApi.getCurrencies().then(function (res) {
+    res.json().then(function (json) {
+        loggger.debug(json)
+    })
+})
+
+let restSpotApi = new okex.RestSpotApi("https://www.okex.com", "", "", "")
+
+restSpotApi.getAccounts().then(function (res) {
+    res.json().then(function (json) {
+        loggger.debug(json)
+    })
+})
+
+let restMarginApi = new okex.RestMarginApi("https://www.okex.com", "", "", "")
+
+restMarginApi.getMarginAccounts().then(function (res) {
+    res.json().then(function (json) {
+        loggger.debug(json)
+    })
+})
+
+let restFuturesApi = new okex.RestFuturesApi("https://www.okex.com", "", "", "")
+
+restFuturesApi.getFuturesPosition().then(function (res) {
+    res.json().then(function (json) {
+        loggger.debug(json)
+    })
+})
+
+let restEttApi = new okex.RestEttApi("https://www.okex.com", "", "", "")
+
+restEttApi.getEttAccounts().then(function (res) {
     res.json().then(function (json) {
         loggger.debug(json)
     })
